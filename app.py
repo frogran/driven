b39b7fc312db87bcaa07eb8b05b2ab1db711e455
frogran <gorfung@gmail.com>
2024-08-21 21:19:17 +0300

    change port in app.py

--- app.py
+++ app.py
@@ -95,5 +95,5 @@ if __name__ == '__main__':
     thread.start()
     
     # Start the Flask app
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port, debug=True)
